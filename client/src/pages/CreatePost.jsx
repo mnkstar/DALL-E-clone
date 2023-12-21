7ee80801afe6952e2c93412160abe4357c351a43
mnkstar <103582688+mnkstar@users.noreply.github.com>
2023-12-21 11:27:49 +0530

    added buttons to generate and share images

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -16,6 +16,8 @@ const CreatePost = () => {
   const handleChange = () => {};
   const handleSurpriseMe = () => {};
 
+  const generateImage = () => {};
+
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -48,6 +50,47 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+          <div className="relative bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus: ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center ">
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.photo}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain"
+              />
+            )}
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className=" mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {generatingImg ? "Generating..." : "Generate"}
+          </button>
+        </div>
+        <div className="mt-10">
+          <p className="font-bold mt-2 text-gray-500 text-[14px]">
+            Once you have create the image you want, you can share it with
+            others in the community.
+          </p>
+          <button
+            type="submit"
+            className="mt-3 text-white font-medium bg-[#6469ff] rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center  "
+          >
+            {loading ? "Sharing..." : "Share with Community"}
+          </button>
         </div>
       </form>
     </section>
